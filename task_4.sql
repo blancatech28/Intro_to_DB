04b1ab15b28077263ae1dc19b53046e1be0bf5cb
Blancatech <Ebenezergyimah52@gmail.com>
2025-10-17 00:33:02 +0000

    fix

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,13 @@
-USE alx_book_store;
 
-SELECT * FROM books;
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
